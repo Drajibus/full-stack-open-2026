5e58d9ded5c7fc73dab2449cfa388dc29ae90d6a
JB F <44908496+Drajibus@users.noreply.github.com>
2026-05-08 00:00:20 +0200

    Answer to 1.8: unicafe step 3

---
+++ part1/unicafe/src/Statistics.jsx
@@ -0,0 +1,20 @@
+const Statistics = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.good + props.neutral + props.bad}</p>
+      <p>
+        average{" "}
+        {(props.good - props.bad) / (props.good + props.neutral + props.bad)}
+      </p>
+      <p>positive {props.good / (props.good + props.neutral + props.bad)} %</p>
+    </div>
+  );
+};
+
+export default Statistics;
