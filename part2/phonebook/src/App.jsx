6a72bce7021b8f7382bffb2b9dceefa3f956f767
JB F <44908496+Drajibus@users.noreply.github.com>
2026-05-12 10:55:12 +0200

    Answer to 2.12: The Phonebook step 7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -31,9 +31,17 @@ const App = () => {
 
     const personObject = { name: newName, number: newNumber };
 
-    setPersons(persons.concat(personObject));
-    setNewName("");
-    setNewNumber("");
+    // setPersons(persons.concat(personObject));
+    // setNewName("");
+    // setNewNumber("");
+
+    axios
+      .post("http://localhost:3001/persons", personObject)
+      .then((response) => {
+        setPersons(persons.concat(personObject));
+        setNewName("");
+        setNewNumber("");
+      });
   };
 
   const personsToShow =
