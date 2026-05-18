c10fca2a23683fe7cc434f2393da2c81ff459285
JB F <44908496+Drajibus@users.noreply.github.com>
2026-05-18 12:06:58 +0200

    Answer to 2.20*: Data for countries, step 3

--- part2/countries/src/components/CountryInfos.jsx
+++ part2/countries/src/components/CountryInfos.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+const api_key = import.meta.env.VITE_SOME_KEY;
+
 const CountryInfos = ({ countryData }) => {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    if (countryData !== null) {
+      const latlng = countryData.capitalInfo.latlng;
+      console.log("GET Weather");
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latlng[0]}&lon=${latlng[1]}&appid=${api_key}`,
+        )
+        .then((response) => {
+          console.log(response.data);
+          const newWeather = {
+            temp: response.data.main.temp - 273.15,
+            img: `https://openweathermap.org/payload/api/media/file/${response.data.weather[0].icon}.png`,
+            alt: response.data.weather[0].description,
+            wind: response.data.wind.speed,
+          };
+          console.log(newWeather);
+          setWeather(newWeather);
+        });
+    }
+  }, [countryData]);
+
   if (countryData === null) {
     return null;
   }
@@ -15,6 +43,10 @@ const CountryInfos = ({ countryData }) => {
         ))}
       </ul>
       <img src={countryData.flags.png} alt={countryData.flags.alt} />
+      <h2>Weather in {countryData.capital}</h2>
+      <p>Temperature {weather.temp} Celsius</p>
+      <img src={weather.img} alt={weather.alt}></img>
+      <p>Wind {weather.wind} m/s</p>
     </div>
   );
 };
