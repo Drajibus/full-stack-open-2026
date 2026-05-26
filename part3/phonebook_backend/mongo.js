a4699ea0fe653b031c87254a3eabe8a30ca118b0
JB F <44908496+Drajibus@users.noreply.github.com>
2026-05-26 11:10:54 +0200

    Answer to 3.12: Command-line database

---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,44 @@
+const { default: mongoose } = require("mongoose");
+const mongo = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://FSOC:${password}@cluster0.hkgnxz7.mongodb.net/?appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+
+    mongoose.connection.close();
+  });
+} else {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${person.name} to phonebook`);
+    mongoose.connection.close();
+  });
+}
