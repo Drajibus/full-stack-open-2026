cc3428e5b7fa026d2d642a8f3e21866bcaada418
JB F <44908496+Drajibus@users.noreply.github.com>
2026-06-06 22:29:40 +0200

    Answer to 4.5*: Fonctions d'aide et tests unitaires, étape 3

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -11,7 +11,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+
+  if (!blogs || blogs.length === 0) return null
+
+  const found = blogs.reduce((favorite, current) => {
+    return current.likes > favorite.likes ? current : favorite
+  }, blogs[0])
+
+  return {
+    title: found.title,
+    author: found.author,
+    likes: found.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
