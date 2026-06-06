10049e8cf8af8fee4375dbe1c16b9c6818ac7914
JB F <44908496+Drajibus@users.noreply.github.com>
2026-06-06 14:04:23 +0200

    Answer to 4.1 Liste de blogs, étape 1

--- part4/blog-backend/index.js
+++ part4/blog-backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -12,8 +14,15 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = "mongodb://localhost/bloglist";
-mongoose.connect(mongoUrl);
+const mongoUrl = config.MONGODB_URI;
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    logger.info("Connected to MongoDB Atlas");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
@@ -32,7 +41,6 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
