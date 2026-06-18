6fc3d5e6a0a6a66e22a52b8d9571e2728a4c9984
JB F <44908496+Drajibus@users.noreply.github.com>
2026-06-18 17:43:24 +0200

    Q4.16 need tests implementation to be completed

---
+++ part4/blog-backend/tests/user_api.test.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+
