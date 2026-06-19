d900d6223862765c3e99f7eb324c9ce8936c7c68
JB F <44908496+Drajibus@users.noreply.github.com>
2026-06-19 16:27:28 +0200

    Still some tests todo

--- part4/blog-backend/tests/user_api.test.js
+++ part4/blog-backend/tests/user_api.test.js
@@ -6,3 +6,89 @@ const api = supertest(app)
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+
+  test('creation fails with proper statuscode and message if username is missing', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: '',
+      name: 'John Missusername',
+      password: 'myusernameismissing'
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(result.body.error).toContain('User validation failed: username: Path `username` is required.')
+    console.log(result.body.error)
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+
+  test.todo('creation fails with proper statuscode and message if username is not long enough (3)')
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(result.body.error).toContain('expected `username` to be unique')
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+
+  test.todo('creation fails with proper statuscode and message if password is missing')
+
+  test.todo('creation fails with proper statuscode and message if password is not long enough (3)')
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
