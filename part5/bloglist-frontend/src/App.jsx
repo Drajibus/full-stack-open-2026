c199fe080ef223f078ae8f442113d78f197c4fd5
JB F <44908496+Drajibus@users.noreply.github.com>
2026-06-26 11:02:16 +0200

    Answer to 5.10: Blog List Frontend, step 10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -148,9 +148,12 @@ const App = () => {
 
       <h2>Blogs</h2>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
-      ))}
+      {blogs
+        .slice()
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
+        ))}
     </div>
   )
 }
