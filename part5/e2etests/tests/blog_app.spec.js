058f5ae35db9de1a64c6092b5c7547e74b7f6510
JB F <44908496+Drajibus@users.noreply.github.com>
2026-06-30 19:07:16 +0200

    Answer to 5.20: Blog List End To End Testing, step 4

--- part5/e2etests/tests/blog_app.spec.js
+++ part5/e2etests/tests/blog_app.spec.js
@@ -1,10 +1,10 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
-const { loginWith } = require("./helper");
+const { loginWith, createBlog } = require("./helper");
 
 describe("Blog app", () => {
   beforeEach(async ({ page, request }) => {
-    await request.post("api/testing/reset");
-    await request.post("api/users", {
+    await request.post("/api/testing/reset");
+    await request.post("/api/users", {
       data: {
         name: "Matti Luukkainen",
         username: "mluukkai",
@@ -15,8 +15,7 @@ describe("Blog app", () => {
   });
 
   test("Login form is shown", async ({ page }) => {
-    const locator = page.getByRole("button", { name: "log in" });
-    await expect(locator).toBeVisible();
+    await expect(page.getByRole("button", { name: "log in" })).toBeVisible();
   });
 
   describe("Login", () => {
@@ -34,21 +33,41 @@ describe("Blog app", () => {
   describe("When logged in", () => {
     beforeEach(async ({ page }) => {
       await loginWith(page, "mluukkai", "salainen");
-      await expect(page.getByText("Connected as")).toBeVisible();
     });
 
     test("a new blog can be created", async ({ page }) => {
-      await page.getByRole("button", { name: "create new blog" }).click();
-      await page
-        .getByRole("textbox", { name: "title" })
-        .fill("a new blog can be created");
-      await page.getByRole("textbox", { name: "author" }).fill("Playwright");
-      await page
-        .getByRole("textbox", { name: "url" })
-        .fill("http://its-a-test.com");
-      await page.getByRole("button", { name: "create" }).click();
-      page.pause();
+      await createBlog(
+        page,
+        "a new blog can be created",
+        "Playwright",
+        "http://its-a-test.com",
+      );
       await expect(page.getByText("Playwright", { exact: true })).toBeVisible();
+    });
+
+    test("a blog can be liked", async ({ page }) => {
+      await createBlog(
+        page,
+        "a new blog can be created",
+        "Playwright",
+        "http://its-a-test.com",
+      );
+      await expect(page.getByText("Playwright", { exact: true })).toBeVisible();
+
+      const blogDiv = page.locator("div").filter({
+        hasText: "a new blog can be created",
+        hasNotText: "New blog created!",
+      });
+
+      await blogDiv.getByRole("button", { name: "view" }).click();
+
+      await blogDiv.getByRole("button", { name: "like" }).click();
+
+      const textAfter = blogDiv
+        .getByRole("button", { name: "like" })
+        .locator("..");
+
+      await expect(textAfter).toContainText("likes 1");
     });
   });
 });
--- part5/e2etests/tests/helper.js
+++ part5/e2etests/tests/helper.js
@@ -5,4 +5,12 @@ const loginWith = async (page, username, password) => {
   await page.getByRole("button", { name: "log in" }).click();
 };
 
-export { loginWith };
+const createBlog = async (page, title, author, url) => {
+  await page.getByRole("button", { name: "create new blog" }).click();
+  await page.getByRole("textbox", { name: "title" }).fill(title);
+  await page.getByRole("textbox", { name: "author" }).fill(author);
+  await page.getByRole("textbox", { name: "url" }).fill(url);
+  await page.getByRole("button", { name: "create" }).click();
+};
+
+export { loginWith, createBlog };
