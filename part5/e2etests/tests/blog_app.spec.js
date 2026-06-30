7d88f977848ce1a71481611241234ecb82080ba3
JB F <44908496+Drajibus@users.noreply.github.com>
2026-06-30 14:40:35 +0200

    Answer to 5.18: Blog List End To End Testing, step 2

--- part5/e2etests/tests/blog_app.spec.js
+++ part5/e2etests/tests/blog_app.spec.js
@@ -1,12 +1,33 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { loginWith } = require("./helper");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:5173");
+  beforeEach(async ({ page, request }) => {
+    await request.post("api/testing/reset");
+    await request.post("api/users", {
+      data: {
+        name: "Matti Luukkainen",
+        username: "mluukkai",
+        password: "salainen",
+      },
+    });
+    await page.goto("/");
   });
 
   test("Login form is shown", async ({ page }) => {
     const locator = page.getByRole("button", { name: "log in" });
     await expect(locator).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await loginWith(page, "mluukkai", "salainen");
+      await expect(page.getByText("Connected as")).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await loginWith(page, "mlukai", "salanen");
+      await expect(page.getByText("Wrong username or password")).toBeVisible();
+    });
   });
 });
