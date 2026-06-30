12217ec92a302dcf7ced02314a753f2bf1879a63
JB F <44908496+Drajibus@users.noreply.github.com>
2026-06-30 15:12:21 +0200

    Answer to 5.19: Blog List End To End Testing, step 3

--- part5/e2etests/tests/blog_app.spec.js
+++ part5/e2etests/tests/blog_app.spec.js
@@ -30,4 +30,25 @@ describe("Blog app", () => {
       await expect(page.getByText("Wrong username or password")).toBeVisible();
     });
   });
+
+  describe("When logged in", () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, "mluukkai", "salainen");
+      await expect(page.getByText("Connected as")).toBeVisible();
+    });
+
+    test("a new blog can be created", async ({ page }) => {
+      await page.getByRole("button", { name: "create new blog" }).click();
+      await page
+        .getByRole("textbox", { name: "title" })
+        .fill("a new blog can be created");
+      await page.getByRole("textbox", { name: "author" }).fill("Playwright");
+      await page
+        .getByRole("textbox", { name: "url" })
+        .fill("http://its-a-test.com");
+      await page.getByRole("button", { name: "create" }).click();
+      page.pause();
+      await expect(page.getByText("Playwright", { exact: true })).toBeVisible();
+    });
+  });
 });
