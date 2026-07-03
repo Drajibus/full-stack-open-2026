8e7d9677ff3bb5b78c5f5eebb5e12b4ff020e20f
JB F <44908496+Drajibus@users.noreply.github.com>
2026-07-03 16:53:54 +0200

    Answer to 5.27: routed blogs, step4

--- part5/e2etests/tests/blog_app.spec.js
+++ part5/e2etests/tests/blog_app.spec.js
@@ -4,6 +4,7 @@ const { loginWith, createBlog } = require("./helper");
 describe("Blog app", () => {
   beforeEach(async ({ page, request }) => {
     await request.post("/api/testing/reset");
+
     await request.post("/api/users", {
       data: {
         name: "Matti Luukkainen",
@@ -11,20 +12,101 @@ describe("Blog app", () => {
         password: "salainen",
       },
     });
+
+    await request.post("/api/users", {
+      data: {
+        name: "Jean Sibelius",
+        username: "jsibel",
+        password: "finlandia",
+      },
+    });
+
+    const loginResponse = await request.post("/api/login", {
+      data: {
+        username: "mluukkai",
+        password: "salainen",
+      },
+    });
+
+    const loginData = await loginResponse.json();
+    const token = loginData.token;
+
+    await request.post("/api/blogs", {
+      data: {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 0,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const loginResponse2 = await request.post("/api/login", {
+      data: {
+        username: "jsibel",
+        password: "finlandia",
+      },
+    });
+
+    const loginData2 = await loginResponse2.json();
+    const token2 = loginData2.token;
+
+    await request.post("/api/blogs", {
+      data: {
+        title: "Writing long musical pieces",
+        author: "A musician",
+        url: "http://listen.com",
+        likes: 0,
+      },
+      headers: {
+        Authorization: `Bearer ${token2}`,
+      },
+    });
+
     await page.goto("/");
   });
 
+  test("Blog information and the number of likes are displayed to unauthenticated users, buttons are not displayed", async ({
+    page,
+  }) => {
+    await page.getByText("Canonical string reduction").click();
+
+    expect(page.getByRole("heading")).toContainText(
+      "Canonical string reduction",
+    );
+    expect(page.getByRole("heading")).toContainText("Edsger W. Dijkstra");
+
+    expect(
+      page.getByRole("link", {
+        name: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      }),
+    ).toBeVisible();
+
+    expect(page.getByText("likes 0")).toBeVisible();
+    expect(page.getByText("Added by Matti Luukkainen")).toBeVisible();
+
+    await expect(page.getByRole("button", { name: "like" })).not.toBeVisible();
+    await expect(
+      page.getByRole("button", { name: "remove" }),
+    ).not.toBeVisible();
+  });
+
   test("Login form is shown", async ({ page }) => {
+    await page.getByRole("link", { name: "login" }).click();
     await expect(page.getByRole("button", { name: "log in" })).toBeVisible();
   });
 
   describe("Login", () => {
     test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByRole("link", { name: "login" }).click();
       await loginWith(page, "mluukkai", "salainen");
       await expect(page.getByText("Connected as")).toBeVisible();
     });
 
     test("fails with wrong credentials", async ({ page }) => {
+      await page.getByRole("link", { name: "login" }).click();
       await loginWith(page, "mlukai", "salanen");
       await expect(page.getByText("Wrong username or password")).toBeVisible();
     });
@@ -32,7 +114,28 @@ describe("Blog app", () => {
 
   describe("When logged in", () => {
     beforeEach(async ({ page }) => {
+      await page.getByRole("link", { name: "login" }).click();
       await loginWith(page, "mluukkai", "salainen");
+    });
+
+    test("Authenticated users who are not the blog’s creator are shown only the like button", async ({
+      page,
+    }) => {
+      await page.getByText("Writing long musical pieces").click();
+
+      await expect(page.getByRole("button", { name: "like" })).toBeVisible();
+      await expect(
+        page.getByRole("button", { name: "remove" }),
+      ).not.toBeVisible();
+    });
+
+    test("The blog’s creator is also shown the delete button", async ({
+      page,
+    }) => {
+      await page.getByText("Canonical string reduction").click();
+
+      await expect(page.getByRole("button", { name: "like" })).toBeVisible();
+      await expect(page.getByRole("button", { name: "remove" })).toBeVisible();
     });
 
     test("a new blog can be created", async ({ page }) => {
